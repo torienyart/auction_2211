a71a064b9bbe552f0881e148e270e8fe7daf0496
Tori Enyart <torienyart@gmail.com>
2023-01-08 07:12:21 -0700

    test: Write tests for Auction initialize and item methods

---
+++ spec/auction_spec.rb
@@ -0,0 +1,27 @@
+require './lib/item'
+require './lib/attendee'
+require './lib/auction'
+
+describe Auction do
+  let(auction){Auction.new}
+
+  it 'exists' do
+    expect(auction).to be an Auction
+  end
+
+  it 'has readable attributes' do
+    expect(auction.items).to eq([])
+  end
+
+  before (:each) do
+    auction.add_item(item1)
+    auction.add_item(item2)
+  end
+  it 'can add_items' do
+    expect(auction.items).to eq(item1, item2)
+  end
+
+  it 'can tell item names' do
+    expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
+end
