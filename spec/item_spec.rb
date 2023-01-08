ce26e8aa9c96a28d7a00214d227ae12e66f8b371
Tori Enyart <torienyart@gmail.com>
2023-01-08 07:50:10 -0700

    test: Write tests for item bid tracking

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,5 @@
 require './lib/item'
+require './lib/attendee'
 
 
 describe Item do
@@ -12,5 +13,27 @@ describe Item do
 
   it 'has readable attributes' do
     expect(item1.name).to eq("Chalkware Piggy Bank")
+  end
+  
+  describe 'bids' do
+    it 'can track bids' do
+      expect(item1.bids).to eq({})
+    end
+
+    it 'can add bids' do
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+
+      expect(item1.bids).to eq({
+        attendee2 => 20,
+        attendee1 => 22
+      })
+    end
+
+  it 'can track highest bid' do
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    expect(item1.current_high_bid).to eq(22)
   end
 end
