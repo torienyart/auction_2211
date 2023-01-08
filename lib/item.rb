d2df7c5b01d609832f231a75190f774dee8bb47e
Tori Enyart <torienyart@gmail.com>
2023-01-08 07:59:59 -0700

    feat: Write methods for item bidding

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,17 @@
 class Item
-  attr_reader :name
+  attr_reader :name, :bids
   def initialize(name)
     @name = name
+    @bids = {}
+  end
+
+  def add_bid(attendee, bid)
+    @bids[attendee] = bid
+  end
+
+  def current_high_bid
+    @bids.values.max_by do |value|
+      value
+    end
   end
 end
