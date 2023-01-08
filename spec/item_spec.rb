95482f0d1ee2f3ba895b03aa5b36cc4301b44e74
Tori Enyart <torienyart@gmail.com>
2023-01-08 07:06:11 -0700

    test: Write tests for initializing new item

---
+++ spec/item_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+
+
+describe Item do
+  let(:item1){Item.new('Chalkware Piggy Bank')}
+  let(:item2){item2 = Item.new('Bamboo Picture Frame')}
+  
+  it 'exists' do
+    expect(item1).to be a Item
+    expect(item2).to be a Item
+  end
+
+  it 'has readable attributes' do
+    expect(item1.name).to eq("Chalkware Piggy Bank")
+  end
+end
