cd0d2b65b072656458a371d22fbdd70750d7254c
Tori Enyart <torienyart@gmail.com>
2023-01-08 07:50:30 -0700

    test: Write tests for auction bid tracking

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -6,6 +6,13 @@ describe Auction do
   let(:auction){Auction.new}
   let(:item1){Item.new('Chalkware Piggy Bank')}
   let(:item2){item2 = Item.new('Bamboo Picture Frame')}
+  let(:item3){Item.new('Homemade Chocolate Chip Cookies')}
+  let(:item4){Item.new('2 Days Dogsitting')}
+  let(:item5){Item.new('Forever Stamps')}
+  let(:attendee1){Attendee.new(name: 'Megan', budget: '$50')}
+  let(:attendee2){Attendee.new(name: 'Bob', budget: '$75')}
+  let(:attendee2){Attendee.new(name: 'Mike', budget: '$100')}
+
 
   it 'exists' do
     expect(auction).to be_an_instance_of Auction
@@ -27,6 +34,32 @@ describe Auction do
 
     it 'can tell item names' do
       expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+  end
+
+  describe 'bidding' do
+    before(:each) do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      item4.add_bid(attendee3, 50)
+    end
+
+    it 'can determine unpopular items' do
+      expect(auction.unpopular_items).to eq[item2, item3, item5]
+
+      item3.add_bid(attendee2, 15)
+
+      expect(auction.unpopular_items).to eq[item2, item5]
+    end
+    
+    it 'can determine potential revenue' do
+      expect(auction.potential_revenue).to eq(87)
     end
   end
 end
